6578f185812e408c1b34a77d629846f76b37a22d
Stephen-Adom <stephen.alaska94@hotmail.com>
2024-02-12 20:26:21 +0000

    install react router dom

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,13 +2,7 @@
 import styles from './app.module.css';
 
 export function App() {
-  return (
-    <div className="text-red-500">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident iste
-      quaerat vero praesentium commodi sequi eius atque, consequuntur omnis qui
-      amet accusantium. Qui, veniam debitis! Quidem quasi minima quisquam modi?
-    </div>
-  );
+  return <main></main>;
 }
 
 export default App;
