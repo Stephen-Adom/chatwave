46fe609923cafa1978fbf6df0382293dc47e4f6e
Stephen-Adom <stephen.alaska94@hotmail.com>
2024-02-16 22:11:58 +0000

    Add react hook forms to register form

--- src/app/pages/auth/register/register.tsx
+++ src/app/pages/auth/register/register.tsx
@@ -1,8 +1,93 @@
+/* eslint-disable @nx/enforce-module-boundaries */
 import whiteBg from 'assets/images/auth/white-bg.jpeg';
 import chatWave from 'assets/images/logo/chatwave-logo.png';
+import { useForm } from 'react-hook-form';
+import { PhoneInput, PhoneInputRefType } from 'react-international-phone';
 import { Link } from 'react-router-dom';
+import { PhoneNumberUtil } from 'google-libphonenumber';
+import { useEffect, useRef } from 'react';
+import { debounceTime, fromEvent, map } from 'rxjs';
+import styles from './register.module.css';
+
+type registerFormType = {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  phonenumber: string;
+};
 
 export const Register = () => {
+  const form = useForm<registerFormType>({
+    defaultValues: {
+      firstname: '',
+      lastname: '',
+      username: '',
+      email: '',
+      phonenumber: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const {
+    watch,
+    control,
+    clearErrors,
+    setError,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = form;
+
+  const phoneUtil = PhoneNumberUtil.getInstance();
+
+  const phoneRef = useRef<PhoneInputRefType>(null);
+
+  const { name } = register('phonenumber', {
+    required: {
+      value: true,
+      message: 'Phonenumber is required',
+    },
+  });
+
+  useEffect(() => {
+    if (phoneRef && phoneRef.current) {
+      fromEvent(phoneRef.current, 'keyup')
+        .pipe(
+          map((event) => {
+            const target = event.target as PhoneInputRefType;
+            return target?.state.phone;
+          }),
+          debounceTime(400)
+        )
+        .subscribe((data) => {
+          if (data) {
+            const isValid = validatePhoneInput(data);
+
+            if (!isValid) {
+              setError('phonenumber', {
+                type: 'phoneInvalid',
+                message: 'Invalid phone number',
+              });
+            } else {
+              clearErrors('phonenumber');
+            }
+          }
+        });
+    }
+  }, [phoneRef.current]);
+
+  const validatePhoneInput = (phone: string) => {
+    try {
+      return phoneUtil.isValidNumber(phoneUtil.parseAndKeepRawInput(phone));
+    } catch (error) {
+      return false;
+    }
+  };
+
   return (
     <div
       id="register-section"
@@ -18,14 +103,28 @@ export const Register = () => {
         </div>
         <h1 className="text-2xl text-center my-5">Welcome To ChatWave!</h1>
 
-        <form className="grid grid-cols-2 gap-x-4 w-[85%] mx-auto mt-10">
+        <form
+          onSubmit={handleSubmit((data) => console.log(data))}
+          className="grid grid-cols-2 gap-x-4 w-[85%] mx-auto mt-10"
+        >
           <div className="form-group">
             <input
               type="text"
               aria-label="firstname"
               placeholder="Firstname"
-              className="form-control"
+              className={`form-control ${
+                control.getFieldState('firstname').invalid && styles.error
+              }`}
+              {...register('firstname', {
+                required: {
+                  value: true,
+                  message: 'Firstname is required',
+                },
+              })}
             />
+            <span className="block text-red-600 italic text-xs mt-2 font-semibold">
+              {errors.firstname?.message}
+            </span>
           </div>
 
           <div className="form-group">
@@ -33,26 +132,84 @@ export const Register = () => {
               type="text"
               aria-label="lastname"
               placeholder="Lastname"
-              className="form-control"
+              className={`form-control ${
+                control.getFieldState('lastname').invalid && styles.error
+              }`}
+              {...register('lastname', {
+                required: {
+                  value: true,
+                  message: 'Lastname is required',
+                },
+              })}
             />
+            <span className="block text-red-600 italic text-xs mt-2 font-semibold">
+              {errors.lastname?.message}
+            </span>
           </div>
 
-          <div className="form-group col-span-2 sm:col-span-2 md:col-span-1">
+          <div className="form-group">
             <input
               type="text"
               aria-label="username"
               placeholder="Username"
-              className="form-control"
+              className={`form-control ${
+                control.getFieldState('username').invalid && styles.error
+              }`}
+              {...register('username', {
+                required: {
+                  value: true,
+                  message: 'Username is required',
+                },
+              })}
             />
+            <span className="block text-red-600 italic text-xs mt-2 font-semibold">
+              {errors.username?.message}
+            </span>
           </div>
 
-          <div className="form-group col-span-2 sm:col-span-2 md:col-span-1">
+          <div className="form-group">
             <input
               type="email"
               aria-label="email"
               placeholder="Email"
-              className="form-control"
+              className={`form-control ${
+                control.getFieldState('email').invalid && styles.error
+              }`}
+              {...register('email', {
+                required: {
+                  value: true,
+                  message: 'Email is required',
+                },
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Invalid email address',
+                },
+              })}
             />
+            <span className="block text-red-600 italic text-xs mt-2 font-semibold">
+              {errors.email?.message}
+            </span>
+          </div>
+
+          <div className="form-group col-span-2">
+            <PhoneInput
+              defaultCountry="gh"
+              name={name}
+              ref={phoneRef}
+              className={`${
+                control.getFieldState('firstname').invalid && styles.error
+              }`}
+            />
+            {errors.phonenumber?.type === 'phoneInvalid' && (
+              <span className="block text-red-600 italic text-xs mt-2 font-semibold">
+                {errors.phonenumber.message}
+              </span>
+            )}
+            {errors.phonenumber?.type === 'required' && (
+              <span className="block text-red-600 italic text-xs mt-2 font-semibold">
+                {errors.phonenumber.message}
+              </span>
+            )}
           </div>
 
           <div className="form-group">
@@ -60,21 +217,57 @@ export const Register = () => {
               type="password"
               aria-label="password"
               placeholder="Password"
-              className="form-control"
+              className={`form-control ${
+                control.getFieldState('password').invalid && styles.error
+              }`}
+              {...register('password', {
+                required: {
+                  value: true,
+                  message: 'Password is required',
+                },
+                minLength: {
+                  value: 8,
+                  message: 'Password must be at least 8 characters',
+                },
+              })}
             />
+
+            <span className="block text-red-600 italic text-xs mt-2 font-semibold">
+              {errors.password?.message}
+            </span>
           </div>
           <div className="form-group">
             <input
               type="password"
               aria-label="confirm_password"
               placeholder="Confirm Password"
-              className="form-control"
+              className={`form-control ${
+                control.getFieldState('confirmPassword').invalid && styles.error
+              }`}
+              {...register('confirmPassword', {
+                required: {
+                  value: true,
+                  message: 'Confirm Password is required',
+                },
+                minLength: {
+                  value: 8,
+                  message: 'Password must be at least 8 characters',
+                },
+                validate: (val: string) => {
+                  if (watch('password') !== val) {
+                    return 'Your passwords do not match';
+                  }
+                },
+              })}
             />
+            <span className="block text-red-600 italic text-xs mt-2 font-semibold">
+              {errors.confirmPassword?.message}
+            </span>
           </div>
 
           <div className="form-group mt-8 col-span-2">
             <button
-              type="button"
+              type="submit"
               className="btn bg-primaryColor hover:bg-secondaryColor text-white w-full py-3 shadow-shadowLight"
             >
               Create Account
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @nx/enforce-module-boundaries */
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import 'primereact/resources/themes/lara-light-cyan/theme.css';
-import './styles.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@chatwave/utils';
+
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import 'react-international-phone/style.css';
+import './styles.css';
 
 import { PrimeReactProvider } from 'primereact/api';
 
